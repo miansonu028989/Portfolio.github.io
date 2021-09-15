a485507f4b1f9ae74f1c7febd6aece02fe445e61
miansonu028989 <miansonu028989@gmail.com>
2021-09-14 21:24:35 -0700

    ""

--- src/Components/Navigation/Navbar.jsx
+++ src/Components/Navigation/Navbar.jsx
@@ -3,7 +3,7 @@ import './navbarStyle.css';
 const Navbar=()=>{
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                 <div class="container-fluid">
                 <a class="navbar-brand" href="#">Portfolio</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
