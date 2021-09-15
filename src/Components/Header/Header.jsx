b3d44bfddd40971752095797bedc59b9e8dd5522
miansonu028989 <miansonu028989@gmail.com>
2021-09-15 07:18:35 -0700

    ""

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,6 +4,7 @@ import ReactTyped from "react-typed";
 const Header=()=>{
     return (
         <div className="container-fluid header-container">
+
             <div className="mian-text header-text">
                 <p className="p-text header-text-p">
                 Websites Developer <span className="span-text">Portfolio</span>
