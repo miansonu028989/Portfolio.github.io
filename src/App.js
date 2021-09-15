1ba342c41a4f22695673432aa0b30b275d06d292
miansonu028989 <miansonu028989@gmail.com>
2021-09-14 20:39:34 -0700

    ""

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navigation/Navbar";
 function App() {
   return (
     <div className="App">
+    <Navbar/>
     </div>
   );
 }
