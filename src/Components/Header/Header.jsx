9cde3c6de25dc9f3abdc9007e057f23f97ee299c
miansonu028989 <miansonu028989@gmail.com>
2021-09-15 08:00:08 -0700

    ""

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import './headerStyle.css';
 import ReactTyped from "react-typed";
+
 const Header=()=>{
     return (
-        <div className="container-fluid header-container" id="particles.js">
+        <div className="container-fluid header-container">
 
-            <div className="mian-text header-text">
+            <div className="mian-text header-text" id="particles.js">
                 <p className="p-text header-text-p">
                 Websites Developer <span className="span-text">Portfolio</span>
                 </p>
